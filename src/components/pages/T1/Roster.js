fe0dfaf1506d2d22ae190dd47f77feec881d5251
이준형 <dlwnsgud261@naver.com>
2023-01-19 17:06:22 +0900

    선수별 컴포넌트 설정

--- src/components/pages/T1/Roster.js
+++ src/components/pages/T1/Roster.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
-import profileOner from '../../../images/T1-player/profile_Oner.jpg';
-import profileFaker from '../../../images/T1-player/profile_Faker.jpg';
-import profileGumayusi from '../../../images/T1-player/profile_Gumayusi.jpg';
-import profileKeria from '../../../images/T1-player/profile_Keria.jpg';
+
 import './Roster.css';
 import styled from "styled-components";
 import StatsOfJeus from './Jeus/StatsOfJeus';
 import MainOfJeus from './Jeus/MainOfJeus';
+import StatsOfOner from './Oner/StatsOfOner';
+import MainOfOner from './Oner/MainOfOner';
+import StatsOfFaker from './Faker/StatsOfFaker';
+import MainOfFaker from './Faker/MainOfFaker';
+import StatsOfGumayusi from './Gumayusi/StatsOfGumayusi';
+import MainOfGumayusi from './Gumayusi/MainOfGumayusi';
+import StatsOfKeria from './Keria/StatsOfKeria';
+import MainOfKeria from './Keria/MainOfKeria';
 
 
 
 const Roster  = () => {
-    const [click, setClick] = useState(false);
+    const [click1, setClick1] = useState(false);
+    const [click2, setClick2] = useState(false);
+    const [click3, setClick3] = useState(false);
+    const [click4, setClick4] = useState(false);
+    const [click5, setClick5] = useState(false);
     
     /*     useEffect(()=>{
             첫 마운트시 메인 효과 실행 안되게
@@ -22,62 +31,38 @@ const Roster  = () => {
     return(
         <div>
             <ul className="player-list">
-                <li className="player-card" >
+                <li className="player-card jeus-image" >
+                    
+                { click1 ? <StatsOfJeus setClick={setClick1} /> : <MainOfJeus setClick={setClick1} /> }
                 
-                    
-                    { click ? <StatsOfJeus setClick={setClick} /> : <MainOfJeus setClick={setClick} /> }
-                    
+                </li>
+
+                <li className="player-card oner-image">
+
+                { click2 ? <StatsOfOner setClick={setClick2} /> : <MainOfOner setClick={setClick2} /> }
 
                 </li>
 
-                <li className="player-card">
-                    <img className="player-image" src={profileOner} alt="Jgl" />
-                    123
-                    <div className="player-container">
-                        <div className="player-intro">
-                            111
-                        </div>
-                    </div>
+                <li className="player-card faker-image">
+
+                { click3 ? <StatsOfFaker setClick={setClick3} /> : <MainOfFaker setClick={setClick3} /> }
 
                 </li>
 
-                <li className="player-card">
-                    <img className="player-image" src={profileFaker} alt="Mid" />
-                    123
-                    <div className="player-container">
-                        <div className="player-intro">
-                            111
-                        </div>
-                    </div>
+                <li className="player-card gumayusi-image">
+
+                { click4 ? <StatsOfGumayusi setClick={setClick4} /> : <MainOfGumayusi setClick={setClick4} /> }
 
                 </li>
 
-                <li className="player-card">
-                    <img className="player-image" src={profileGumayusi} alt="Adc" />
-                    123
-                    <div className="player-container">
-                        <div className="player-intro">
-                            111
-                        </div>
-                    </div>
+                <li className="player-card keria-image">
+
+                { click5 ? <StatsOfKeria setClick={setClick5} /> : <MainOfKeria setClick={setClick5} /> }
 
                 </li>
-
-                <li className="player-card">
-                    <img className="player-image" src={profileKeria} alt="Spt" />
-                    123
-                    <div className="player-container">
-                        <div className="player-intro">
-                            111
-                        </div>
-                    </div>
-                </li>
-
             </ul>
             
-        
-        
-            </div> 
+        </div> 
     )
             
            
