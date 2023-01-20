9f03855df058987eec5c151b5ac9487c1789b698
이준형 <dlwnsgud261@naver.com>
2023-01-20 18:06:43 +0900

    Roster화면 반응형 설정

--- src/components/pages/T1/Roster.js
+++ src/components/pages/T1/Roster.js
@@ -22,7 +22,7 @@ const Roster  = () => {
     const [click2, setClick2] = useState(false);
     const [click3, setClick3] = useState(false);
     const [click4, setClick4] = useState(false);
-    const [click5, setClick5] = useState(false);
+    const [click5, setClick5] = useState(false); 
     
     /*     useEffect(()=>{
             첫 마운트시 메인 효과 실행 안되게
@@ -33,31 +33,31 @@ const Roster  = () => {
             <ul className="player-list">
                 <li className="player-card jeus-image" >
                     
-                { click1 ? <StatsOfJeus setClick={setClick1} /> : <MainOfJeus setClick={setClick1} /> }
+                { click1 ? <StatsOfJeus setClick1={setClick1} /> : <MainOfJeus setClick1={setClick1} /> }
                 
                 </li>
 
                 <li className="player-card oner-image">
 
-                { click2 ? <StatsOfOner setClick={setClick2} /> : <MainOfOner setClick={setClick2} /> }
+                { click2 ? <StatsOfOner setClick2={setClick2} /> : <MainOfOner setClick2={setClick2} /> }
 
                 </li>
 
                 <li className="player-card faker-image">
 
-                { click3 ? <StatsOfFaker setClick={setClick3} /> : <MainOfFaker setClick={setClick3} /> }
+                { click3 ? <StatsOfFaker setClick3={setClick3} /> : <MainOfFaker setClick3={setClick3} /> }
 
                 </li>
 
                 <li className="player-card gumayusi-image">
 
-                { click4 ? <StatsOfGumayusi setClick={setClick4} /> : <MainOfGumayusi setClick={setClick4} /> }
+                { click4 ? <StatsOfGumayusi setClick4={setClick4} /> : <MainOfGumayusi setClick4={setClick4} /> }
 
                 </li>
 
                 <li className="player-card keria-image">
 
-                { click5 ? <StatsOfKeria setClick={setClick5} /> : <MainOfKeria setClick={setClick5} /> }
+                { click5 ? <StatsOfKeria setClick5={setClick5} /> : <MainOfKeria setClick5={setClick5} /> }
 
                 </li>
             </ul>
