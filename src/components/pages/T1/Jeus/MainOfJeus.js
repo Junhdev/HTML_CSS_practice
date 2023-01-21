d748fa1c6a61328ad0e7e8482d6c00415887ccb3
이준형 <dlwnsgud261@naver.com>
2023-01-21 23:57:07 +0900

    제우스 메인화면 수정

--- src/components/pages/T1/Jeus/MainOfJeus.js
+++ src/components/pages/T1/Jeus/MainOfJeus.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import './designOfJeus.css';
-
+let isFirst = false
 function MainOfJeus(props){
+    useEffect(() => {
+        if(isFirst){
+          isFirst = true
+          return
+        }
+  
+      //your code that don't want to execute at first time
+    },[])
     
 
     return(
         <div className="player-box">
-            <button onClick={()=>{props.setClick(true)}} className="bttn">버튼</button>
+            <button onClick={()=>{props.setClick1(true)}} className="bttn">버튼</button>
             <div className="player-line">  
                 TOP
                 <div className="player-id">
